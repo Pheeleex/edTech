d2539b02bad317fc4df1a7172e0e43e53e18dd2b
Pheeleex <ohemufelix@gmail.com>
2025-01-21 05:36:17 +0100

    alumni

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,6 @@ import Alumni from "@/components/Alumni";
 import Certifications from "@/components/Certification";
 import Cta from "@/components/Cta";
 import Hero from "@/components/Hero";
-import HowWeWork from "@/components/HowWeWork";
 import Process from "@/components/Process";
 import Schools from "@/components/Schools";
 
